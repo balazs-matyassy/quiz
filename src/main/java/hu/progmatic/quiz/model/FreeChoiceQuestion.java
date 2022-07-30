82bcef791d5568c8a1463a8e7025d7a68b23458a
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-07-30 13:18:16 +0200

    bm: model (FreeChoice)

---
+++ src/main/java/hu/progmatic/quiz/model/FreeChoiceQuestion.java
@@ -0,0 +1,36 @@
+package hu.progmatic.quiz.model;
+
+public class FreeChoiceQuestion {
+    private Long id;
+
+    private String question;
+
+    private String answer;
+
+    public FreeChoiceQuestion() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
