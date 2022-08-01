2a13f85ad1ce1031a278d7f86c1eca263fdadefc
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-01 19:45:06 +0200

    bm: log

---
+++ src/main/java/hu/progmatic/quiz/model/LogMessage.java
@@ -0,0 +1,30 @@
+package hu.progmatic.quiz.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class LogMessage {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String message;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
